bd6918233db300e07802a37bdff8e94b7c583f80
Mike Glabay <java_guru@live.com>
2022-03-16 08:19:26 -0300

    Base NetworkException Handling to pass the error that would normally be printed to the console, to be sent via JSON to the Glabay-Studios WebServer API to Error logging and Software Imrovements - Removed Maven Dependancey of OpenJFX in favor of creating the Artifact for packaging an MSI for an Exe app
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/xyz/glabaystudios/Launcher.java
@@ -0,0 +1,10 @@
+package xyz.glabaystudios;
+
+import xyz.glabaystudios.web.LilBlu;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		LilBlu.main(args);
+	}
+}
--- src/main/java/xyz/glabaystudios/net/NetworkExceptionHandler.java
+++ src/main/java/xyz/glabaystudios/net/NetworkExceptionHandler.java
@@ -1,9 +1,49 @@
 package xyz.glabaystudios.net;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class NetworkExceptionHandler {
 
 	public static void handleException(String errorDesc, Exception thrownException) {
 		System.out.println(errorDesc);
-		if (thrownException != null) thrownException.printStackTrace();
+		if (thrownException == null) {
+			System.out.println("Error message was null.");
+			return;
+		}
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			String errorJson = mapper.writeValueAsString(thrownException.getCause());
+			prepareHttpRequest(errorJson);
+		} catch (JsonProcessingException ignored) {}
+		System.out.println(errorDesc);
+	}
+
+	private static final HttpClient httpClient = HttpClient.newBuilder()
+			.version(HttpClient.Version.HTTP_2)
+			.connectTimeout(Duration.ofSeconds(10))
+			.build();
+
+	private static void prepareHttpRequest(String json) {
+		System.out.println("HTTP POST\n" + json);
+		HttpRequest request = HttpRequest.newBuilder()
+				.POST(HttpRequest.BodyPublishers.ofString(json))
+				.uri(URI.create("http://localhost:8080/lilblu/api/error")) // TODO
+				.setHeader("User-Agent", "Little Blue Buddy - Exception") // add request header
+				.header("Content-Type", "application/json")
+				.build();
+		try {
+			httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
