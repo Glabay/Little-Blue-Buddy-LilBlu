afc1ac3195f0b4eec77be8d2aab349cc1a60dbf3
Mike Glabay <java_guru@live.com>
2022-04-11 08:24:54 -0300

    DefaultCrawler has been updated to include looking for Images
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/xyz/glabaystudios/web/crawler/ecomm/stores/DefaultCrawler.java
+++ src/main/java/xyz/glabaystudios/web/crawler/ecomm/stores/DefaultCrawler.java
@@ -1,5 +1,10 @@
 package xyz.glabaystudios.web.crawler.ecomm.stores;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import xyz.glabaystudios.web.crawler.ecomm.EcommCrawler;
 
@@ -18,42 +23,57 @@ public class DefaultCrawler extends EcommCrawler {
 	public void crawlThePageForContent() {
 		if (page == null) return;
 		System.out.println("<-|-> DEFAULT <-|->");
-		Elements productOption =    page.select("div.product-option-group.form-group");
 		filterProductBasicInfo();
-		addImages(page.select("div.images-wrapper img"));
-		filterProductOptions(productOption);
-		filterProductsForPriceAdjustments();
+		filterProductOptions();
+		filterProductImages();
+		if (this.getPegaProduct().getProductOptions().size() > 0) {
+			try {
+				filterProductsForPriceAdjustments();
+			} catch (ParseException ignore) {
+			}
+		}
 	}
 
-	protected void filterProductsForPriceAdjustments() {
-		String oneLiner = page.select("form.form-vertical script").toString()
-				.split("= \\{")[1]
-				.replace("</script>", "")
-				.replace("} || {};", "")
-				.trim();
-//		System.out.println(oneLiner);
-		List<String> options = List.of(oneLiner.split("},"));
-//		Collections.sort(options);
-		filterOverProducts(options);
+	protected void filterProductsForPriceAdjustments() throws ParseException {
+		List<String> options;
+
+		String oneLiner = page.select("form.form-vertical script").toString();
+		try {
+			oneLiner = oneLiner.split("= \\{")[1].replace("</script>", "").replace("} || {};", "").trim();
+			options = List.of(oneLiner.split("},"));
+			filterOverProducts(options);
+		} catch (Exception ignore) {
+			Elements jsonData = page.select("form.variations_form.cart");
+			String jsonStr = jsonData.attr("data-product_variations");
+
+			JSONParser jsonParser = new JSONParser();
+			JSONArray jsonArray = (JSONArray) jsonParser.parse(jsonStr);
+			for (String key : getPegaProduct().getProductOptions().keySet()) {
+				List<String> option = this.getPegaProduct().getProductOptions().get(key);
+				List<String> optionChoices = new ArrayList<>();
+				for (int i = 0; i < jsonArray.size(); i++) {
+					JSONObject jsonObject = (JSONObject) jsonParser.parse(jsonArray.get(i).toString());
+					double finalPrice = formatPrice(cleanPrice(jsonObject.get("display_regular_price").toString())) - this.getPegaProduct().getProductPriceBase();
+					String formatted = String.format("%s %s %s", option.get(i), (finalPrice > 0.0 ? "+" : "-"), ("$" + decimalFormat.format(finalPrice)));
+					System.out.println(formatted);
+					optionChoices.add(formatted);
+				}
+				this.getPegaProduct().getProductOptionPriceAdjustments().put(key, optionChoices);
+			}
+		}
+
 	}
 
 	protected void filterOverProducts(List<String> list) {
-		// for each option of the product
 		AtomicInteger i = new AtomicInteger(0);
 		for (String key : this.getPegaProduct().getProductOptions().keySet()) {//collect the available choices for this option
 			List<String> option = this.getPegaProduct().getProductOptions().get(key);
-//			System.out.println(key);
-			// new lst to store the formatted choices with adjusted prices
 			List<String> optionChoices = new ArrayList<>();
-//			System.out.println("<-|-> " + list);
 			for (String opt : option) {
-				System.out.println(opt);
 				if (opt.startsWith("-- ")) continue;
-				System.out.println(list.get(Math.min(i.get(), list.size()-1)));
 				String prodPrice = list.get(Math.min(i.get(), list.size()-1)).split(",")[2];
 				double finalPrice = (formatPrice(cleanPrice(prodPrice)) - this.getPegaProduct().getProductPriceBase());
 				String formatted = String.format("%s %s %s", opt, (finalPrice > 0.0 ? "+" : "-"), ("$" + decimalFormat.format(finalPrice)));
-
 //				System.out.println(formatted);
 				optionChoices.add(formatted);
 				i.getAndIncrement();
@@ -62,31 +82,81 @@ public class DefaultCrawler extends EcommCrawler {
 		}
 	}
 
-	protected void filterProductOptions(Elements itemOptions) {
-//		System.out.println("******** NEXT - OPTION ELEMENTS ********");
-		itemOptions.forEach(option -> {
-			String optionName = option.attr("data-name");
-//			System.out.println("Product OptionName: <-|-> " + optionName);
-			List<String> optionChoices = new ArrayList<>();
-			Elements choices = option.select("select.product-option-select option");
-//			System.out.println("Product Options: <-|-> " + choices);
-			if (choices.isEmpty()) {
-				choices = option.select("select.list-option-select option");
-//				System.out.println("Product Options-two: <-|-> " + choices);
-			}
-			choices.forEach(choice -> optionChoices.add(choice.text()));
-			Collections.sort(optionChoices);
-			this.getPegaProduct().getProductOptions().put(optionName, optionChoices);
-		});
+	protected void filterProductOptions() {
+		Elements itemOptions = page.select("div.product-option-group.form-group");
+		if (!itemOptions.isEmpty()) {
+			itemOptions.forEach(option -> {
+				String optionName = option.attr("data-name");
+				List<String> optionChoices = new ArrayList<>();
+				Elements choices = option.select("select.product-option-select option");
+				if (choices.isEmpty()) choices = option.select("select.list-option-select option");
+				choices.forEach(choice -> optionChoices.add(choice.text()));
+				Collections.sort(optionChoices);
+				this.getPegaProduct().getProductOptions().put(optionName, optionChoices);
+			});
+		} else {
+			itemOptions = page.select("table.variations");
+			itemOptions.forEach(option -> {
+				String optionName = option.select("th.label").text();
+				List<String> optionChoices = new ArrayList<>();
+				Elements choices = option.select("td.value option");
+				for (Element ele : choices) {
+					String eleTxt = ele.text().toLowerCase();
+					if (eleTxt.startsWith("choose") || eleTxt.startsWith("select")) continue;
+					optionChoices.add(ele.text());
+				}
+				this.getPegaProduct().getProductOptions().put(optionName, optionChoices);
+			});
+		}
 	}
 
 	protected void filterProductBasicInfo() {
+		addImages(page.select("div.images-wrapper img"));
+		scrapeProductName();
+		checkForSale();
+		scrapeDescription();
+		scrapeAdditionalInformation();
+	}
+
+	private void filterProductImages() {
+		Elements imageElements = page.select("div.images-wrapper img");
+		if (imageElements.isEmpty()) {
+			imageElements = page.select("div.images img");
+		}
+		addImages(imageElements);
+	}
+
+	private void scrapeProductName() {
 		String title = page.select("h1.single-product-title").text();
+		if (title.isEmpty()) title = page.select("div.summary.entry-summary span.headline").text();
+
 //		System.out.println("Product Title: <-|-> " + title);
 		this.getPegaProduct().setProductName(title);
+	}
 
-		checkForSale();
-		this.getPegaProduct().setProductDescription(page.select("div.single-product-description ").text());
+	private void scrapeDescription() {
+		String description = page.select("div.single-product-description ").text();
+		StringBuilder descBuilder = new StringBuilder();
+		if (description.isEmpty()) {
+			descBuilder.append(page.select("div.woocommerce-product-details__short-description").text()).append("\n");
+			descBuilder.append(page.select("div.tabPane").select("div.bt_bb_wrapper").text());
+			description = descBuilder.toString().trim();
+		}
+
+//		System.out.println("Product Description: <-|-> " + description);
+		this.getPegaProduct().setProductDescription(description);
+	}
+
+	private void scrapeAdditionalInformation() {
+		Elements additional = page.select("table.woocommerce-product-attributes.shop_attributes");
+
+		for (Element ele : additional) {
+			String header = ele.select("th.woocommerce-product-attributes-item__label").text();
+			String data = ele.select("td.woocommerce-product-attributes-item__value").text();
+			if (header.equalsIgnoreCase("weight")) getPegaProduct().setProductWeight(data);
+
+//			System.out.println("Product Additional Information: <-|-> " + header + " | " + data);
+		}
 	}
 
 	private void checkForSale() {
@@ -101,6 +171,7 @@ public class DefaultCrawler extends EcommCrawler {
 
 	private void scrapePrice() {
 		String price = page.select("div.single-product-price").attr("data-price");
+		if (price.isEmpty()) price = page.select("p.price span.woocommerce-Price-amount.amount").text();
 		this.getPegaProduct().setProductPriceBase(formatPrice(cleanPrice(price)));
 		if (this.getPegaProduct().isOnSale()) {
 			String salePrice = page.select("div.single-product-price").attr("data-price-sale");
